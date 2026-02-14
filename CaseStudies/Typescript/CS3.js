fab6c01d3acdc6c178daa6b6eedb307d8c604210
Kartik Jain <koderkartik@gmail.com>
2026-02-14 23:33:21 +0530

    Create CS3.js

---
+++ CaseStudies/Typescript/CS3.js
@@ -0,0 +1,7 @@
+var city = "Jabalpur";
+var temperature = 30;
+var isRaining = false;
+function weatherReport(city, temperature, isRaining) {
+    console.log("In " + city + ", it is " + temperature + "°C. Is it raining? " + isRaining);
+}
+weatherReport(city, temperature, isRaining);
