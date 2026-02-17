3473f72d2a52bacbe1828a56997cbb4765a8c15d
Kartik Jain <koderkartik@gmail.com>
2026-02-17 08:02:12 +0530

    Create CS4.ts

---
+++ CaseStudies/Typescript/CS4.ts
@@ -0,0 +1,12 @@
+let score = 25;
+if(true)
+{
+    let score = 50;
+    console.log(score);
+}
+
+const Country = "India";
+//Country = "USA"; Error: Cannot assign to 'Country' because it is a constant.
+
+score = 30;
+console.log(score);
